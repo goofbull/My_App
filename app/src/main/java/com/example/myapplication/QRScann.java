c1a6ea5b439abad2040c7ae358de8e37baabbabe
goofbull <tordxie20@gmail.com>
2024-05-25 23:39:00 +0300

    Added butttons plus background colour

--- app/src/main/java/com/example/myapplication/QRScann.java
+++ app/src/main/java/com/example/myapplication/QRScann.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanOptions;
@@ -18,10 +19,22 @@ public class QRScann extends AppCompatActivity {
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrscann);
+        configureBackButton();
         btn_scan = findViewById(R.id.btn_scan);
         btn_scan.setOnClickListener(v->
         {
             scanCode();
+        });
+    }
+
+    private void configureBackButton()
+    {
+        Button backButton = (Button) findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                finish();
+            }
         });
     }
 
